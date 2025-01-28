8b686b28d3926288b088dc0b29dc82da74db0173
klecio.batista <klecio.batista@interligados.com.br>
2025-01-27 22:37:49 -0300

    ajustes

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,6 +1,6 @@
 <x-layout bodyClass="bg-gray-200">
 
-        <div class="container position-sticky z-index-sticky top-0">
+        {{-- <div class="container position-sticky z-index-sticky top-0">
             <div class="row">
                 <div class="col-12">
                     <!-- Navbar -->
@@ -8,39 +8,19 @@
                     <!-- End Navbar -->
                 </div>
             </div>
-        </div>
+        </div> --}}
         <main class="main-content  mt-0">
             <div class="page-header align-items-start min-vh-100"
                 style="background-image: url('https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80');">
                 <span class="mask bg-gradient-dark opacity-6"></span>
-                <div class="container mt-5">
+                <div class="container">
                     <div class="row signin-margin">
                         <div class="col-lg-4 col-md-8 col-12 mx-auto">
                             <div class="card z-index-0 fadeIn3 fadeInBottom">
                                 <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                                    <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
+                                    <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1 align-items-center mx-auto">
                                         <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Sign in</h4>
-                                        <div class="row mt-3">
-                                            <h6 class='text-white text-center'>
-                                                <span class="font-weight-normal">Email:</span> admin@material.com
-                                                <br>
-                                                <span class="font-weight-normal">Password:</span> secret</h6>
-                                            <div class="col-2 text-center ms-auto">
-                                                <a class="btn btn-link px-3" href="javascript:;">
-                                                    <i class="fa fa-facebook text-white text-lg"></i>
-                                                </a>
-                                            </div>
-                                            <div class="col-2 text-center px-1">
-                                                <a class="btn btn-link px-3" href="javascript:;">
-                                                    <i class="fa fa-github text-white text-lg"></i>
-                                                </a>
-                                            </div>
-                                            <div class="col-2 text-center me-auto">
-                                                <a class="btn btn-link px-3" href="javascript:;">
-                                                    <i class="fa fa-google text-white text-lg"></i>
-                                                </a>
-                                            </div>
-                                        </div>
+                                        <img src="{{ asset('assets') }}/img/esterni/logo.png" style="width: 70%" />
                                     </div>
                                 </div>
                                 <div class="card-body">
@@ -67,24 +47,19 @@
                                             <input type="password" class="form-control" name="password" value='{{ 'secret'}}'>
                                         </div>
                                         @error('password')
-                                        <p class='text-danger inputerror'>{{ $message }} </p>
+                                            <p class='text-danger inputerror'>{{ $message }} </p>
                                         @enderror
-                                        <div class="form-check form-switch d-flex align-items-center my-3">
-                                            <input class="form-check-input" type="checkbox" id="rememberMe">
-                                            <label class="form-check-label mb-0 ms-2" for="rememberMe">Remember
-                                                me</label>
-                                        </div>
                                         <div class="text-center">
                                             <button type="submit" class="btn bg-gradient-primary w-100 my-4 mb-2">Sign
                                                 in</button>
                                         </div>
                                         <p class="mt-4 text-sm text-center">
-                                            Don't have an account?
+                                            Já tem conta?
                                             <a href="{{ route('register') }}"
                                                 class="text-primary text-gradient font-weight-bold">Sign up</a>
                                         </p>
                                         <p class="text-sm text-center">
-                                            Forgot your password? Reset your password
+                                            Esqueceu sua senha? Redefina sua senha
                                             <a href="{{ route('verify') }}"
                                                 class="text-primary text-gradient font-weight-bold">here</a>
                                         </p>
