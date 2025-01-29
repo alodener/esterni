92f87bd91a481d9d0d0aa52c74e4335a705d0175
klecio.batista <klecio.batista@interligados.com.br>
2025-01-28 22:26:03 -0300

    feature: fluxo do cliente

--- resources/views/components/navbars/navs/auth.blade.php
+++ resources/views/components/navbars/navs/auth.blade.php
@@ -1,6 +1,6 @@
 @props(['titlePage'])
 
-<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur"
+<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl position-sticky blur shadow-blur mt-4 left-auto top-1 z-index-sticky" id="navbarBlur"
     navbar-scroll="true">
     <div class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
@@ -13,8 +13,8 @@
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
             <div class="ms-md-auto pe-md-3 d-flex align-items-center">
                 <div class="input-group input-group-outline">
-                    <label class="form-label">Type here...</label>
-                    <input type="text" class="form-control">
+                    {{-- <label class="form-label">Type here...</label>
+                    <input type="text" class="form-control"> --}}
                 </div>
             </div>
             <form method="POST" action="{{ route('logout') }}" class="d-none" id="logout-form">
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,32 +1,17 @@
-<x-layout bodyClass="bg-gray-200">
-
-        {{-- <div class="container position-sticky z-index-sticky top-0">
-            <div class="row">
-                <div class="col-12">
-                    <!-- Navbar -->
-                    <x-navbars.navs.guest signin='login' signup='register'></x-navbars.navs.guest>
-                    <!-- End Navbar -->
-                </div>
-            </div>
-        </div> --}}
-        <main class="main-content  mt-0">
-            <div class="page-header align-items-start min-vh-100"
-                style="background-image: url('https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80');">
-                <span class="mask bg-gradient-dark opacity-6"></span>
-                <div class="container">
-                    <div class="row signin-margin">
-                        <div class="col-lg-4 col-md-8 col-12 mx-auto">
-                            <div class="card z-index-0 fadeIn3 fadeInBottom">
-                                <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                                    <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1 align-items-center mx-auto">
-                                        <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Sign in</h4>
-                                        <img src="{{ asset('assets') }}/img/esterni/logo.png" style="width: 70%" />
-                                    </div>
-                                </div>
-                                <div class="card-body">
-                                    <form role="form" method="POST" action="{{ route('login') }}" class="text-start">
-                                        @csrf
-                                        @if (Session::has('status'))
+<x-layout bodyClass="">
+    <main class="main-content" style="margin-top: 100px">
+        <div class="page-header align-items-start min-vh-100">
+            <div class="container">
+                <div class="row signin-margin">
+                    <div class="col-lg-4 col-md-8 col-12 mx-auto">
+                        <div class="">
+                            <div class="">
+                                <center>
+                                    <img src="{{ asset('assets') }}/img/esterni/logo.png" style="width: 70%" />
+                                </center>
+                                <form role="form" method="POST" action="{{ route('login') }}" class="text-start mt-4">
+                                    @csrf
+                                    @if (Session::has('status'))
                                         <div class="alert alert-success alert-dismissible text-white" role="alert">
                                             <span class="text-sm">{{ Session::get('status') }}</span>
                                             <button type="button" class="btn-close text-lg py-3 opacity-10"
@@ -34,56 +19,49 @@
                                                 <span aria-hidden="true">&times;</span>
                                             </button>
                                         </div>
-                                        @endif
-                                        <div class="input-group input-group-outline mt-3">
-                                            <label class="form-label">Email</label>
-                                            <input type="email" class="form-control" name="email" value="{{ 'admin@material.com' }}">
-                                        </div>
-                                        @error('email')
+                                    @endif
+                                    <div class="input-group input-group-outline mt-3">
+                                        <label class="form-label">Email</label>
+                                        <input type="email" class="form-control" name="email"
+                                            value="{{ 'admin@material.com' }}">
+                                    </div>
+                                    @error('email')
                                         <p class='text-danger inputerror'>{{ $message }} </p>
-                                        @enderror
-                                        <div class="input-group input-group-outline mt-3">
-                                            <label class="form-label">Password</label>
-                                            <input type="password" class="form-control" name="password" value='{{ 'secret'}}'>
-                                        </div>
-                                        @error('password')
-                                            <p class='text-danger inputerror'>{{ $message }} </p>
-                                        @enderror
-                                        <div class="text-center">
-                                            <button type="submit" class="btn bg-gradient-primary w-100 my-4 mb-2">Sign
-                                                in</button>
-                                        </div>
-                                        <p class="mt-4 text-sm text-center">
-                                            Já tem conta?
-                                            <a href="{{ route('register') }}"
-                                                class="text-primary text-gradient font-weight-bold">Sign up</a>
-                                        </p>
-                                        <p class="text-sm text-center">
-                                            Esqueceu sua senha? Redefina sua senha
-                                            <a href="{{ route('verify') }}"
-                                                class="text-primary text-gradient font-weight-bold">here</a>
-                                        </p>
-                                    </form>
-                                </div>
+                                    @enderror
+                                    <div class="input-group input-group-outline mt-3">
+                                        <label class="form-label">Password</label>
+                                        <input type="password" class="form-control" name="password"
+                                            value='{{ 'secret' }}'>
+                                    </div>
+                                    @error('password')
+                                        <p class='text-danger inputerror'>{{ $message }} </p>
+                                    @enderror
+                                    <div class="text-center">
+                                        <button type="submit" class="btn bg-gradient-primary my-4 mb-2">
+                                            Entrar
+                                        </button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
-                <x-footers.guest></x-footers.guest>
             </div>
-        </main>
-        @push('js')
-<script src="{{ asset('assets') }}/js/jquery.min.js"></script>
-<script>
-    $(function() {
+            {{-- <x-footers.guest></x-footers.guest> --}}
+        </div>
+    </main>
+    @push('js')
+        <script src="{{ asset('assets') }}/js/jquery.min.js"></script>
+        <script>
+            $(function() {
 
-    var text_val = $(".input-group input").val();
-    if (text_val === "") {
-      $(".input-group").removeClass('is-filled');
-    } else {
-      $(".input-group").addClass('is-filled');
-    }
-});
-</script>
-@endpush
+                var text_val = $(".input-group input").val();
+                if (text_val === "") {
+                    $(".input-group").removeClass('is-filled');
+                } else {
+                    $(".input-group").addClass('is-filled');
+                }
+            });
+        </script>
+    @endpush
 </x-layout>
