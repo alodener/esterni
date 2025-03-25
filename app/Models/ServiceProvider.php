99c46e9676c8d6fa7a911da423025bd30afbca11
klecio.batista <klecio.batista@interligados.com.br>
2025-03-25 10:25:19 -0300

    fix: media geral estava sendo calculada errada

--- app/Models/ServiceProvider.php
+++ app/Models/ServiceProvider.php
@@ -311,7 +311,6 @@ class ServiceProvider extends Model
             $this->occupational_healthSafety_score,
             $this->occupational_trainings_score
         ];
-        dd($scores, array_sum($scores), round(array_sum($scores) / 8));
 
         return round(array_sum($scores) / 8);
     }
